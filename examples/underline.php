fbe45e8f1cd14a58763d0030e93c00b9e4d4df70
vench <nowwrit@mail.ru>
2016-03-24 09:31:24 +0300

    Fix UnderLineStyle +

---
+++ examples/underline.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* A simple example of creating a document.
+* In the document added header and added two paragraphs with underline. 
+*/
+
+require_once '../vsword/VsWord.php'; 
+VsWord::autoLoad();
+
+$doc = new VsWord();
+$body = $doc->getDocument()->getBody();
+
+$title = new PCompositeNode();
+$rTitle = new RCompositeNode();
+$title->addNode($rTitle); 
+$rTitle->addTextStyle(new BoldStyleNode());
+$rTitle->addTextStyle(new FontSizeStyleNode(36));
+$rTitle->addText("Header 1");
+$body->addNode( $title );
+
+$paragraph = new PCompositeNode();
+$rParagraph= new RCompositeNode();
+$paragraph->addNode($rParagraph);  
+$rParagraph->addTextStyle(new FontSizeStyleNode(14));
+$rParagraph->addText("Some more text ... More text about... Some more text ... More text about... Some more text ... More text about...");
+$body->addNode( $paragraph );
+
+$paragraph2 = new PCompositeNode();
+$rParagraph2 = new RCompositeNode();
+$paragraph2->addNode($rParagraph2);  
+$rParagraph2->addTextStyle(new FontSizeStyleNode(14));
+$rParagraph2->addText("Some more text ... More text about... Some more text ... More text about... Some more text ... More text about...");
+ 
+
+$rParagraph3 = new RCompositeNode();
+$paragraph2->addNode($rParagraph3); 
+$rParagraph3->addTextStyle(new UnderlineStyleNode());
+$rParagraph3->addText('Italic text and underline');
+
+$body->addNode( $paragraph2 );
+ 
+
+$doc->saveAs('./base-ul.docx');
--- vsword/node/UnderlineStyleNode.php
+++ vsword/node/UnderlineStyleNode.php
@@ -2,12 +2,14 @@
 
 /**
 *  Class UnderlineStyleNode
-*  @version 1.0.0
+
+*  @version 1.0.1
 *  @author v.raskin
  * @package vsword.node
 */
 class UnderlineStyleNode extends Node implements INodeStyle {
+	public $style = 'single';
 	public function getWord() {
-		return '<w:u/>';
+		return '<w:u w:val="' . $this->style.'"/>';
 	}
 }
